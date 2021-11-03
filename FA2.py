0cf1f9da6d0a05a94ce57a02185ff653cdcc6466
Antoine Brûlé <antoine.brule@netc.eu>
2021-11-03 22:01:52 +0100

    add lock test

--- FA2.py
+++ FA2.py
@@ -821,6 +821,84 @@ def add_test(config, is_default = True):
         c3.set_pause(False).run(sender = alice, valid = False)
 
 
+    def set_and_test_base_uri(stringUrl, scenario, contract, sender, valid = True):
+        url = sp.bytes('0x' + ''.join([hex(ord(c))[2:] for c in stringUrl]))
+        contract.set_base_uri(url).run(sender = sender, valid = valid)
+        expectedUri = sp.bytes('0x' + ''.join([hex(ord(c))[2:] for c in stringUrl + '0']))
+        resultingUri = contract.token_metadata(0).token_info['']
+        if (valid):
+            scenario.verify(resultingUri == expectedUri)
+        else:
+            scenario.verify(resultingUri != expectedUri)
+
+
+    @sp.add_test(name = "Lock test", is_default = is_default)
+    def lock_test():
+        scenario = sp.test_scenario()
+        scenario.h1("Lock test")
+        scenario.table_of_contents()
+
+        admin, [alice, bob] = get_addresses()
+
+        config.max_editions = 10000
+
+        c1 = FA2(config = config,
+            metadata = sp.utils.metadata_of_url("https://example.com"),
+            admin = admin.address)
+        scenario += c1
+
+        scenario.h2("Initial minting")
+        minted = c1.mint(1).run(sender = alice, amount = sp.mutez(1000000))
+
+        scenario.h3("set_base_uri without lock")
+        set_and_test_base_uri('https://example1.com/api/', scenario, c1, admin)
+
+        scenario.h2("Lock")
+
+        scenario.h3("Lock from non-admin")
+        c1.lock().run(sender = alice, valid = False)
+
+        scenario.h3("set_base_uri still possible")
+        set_and_test_base_uri('https://example2.com/api/', scenario, c1, admin)
+
+        scenario.h3("Lock from admin")
+        c1.lock().run(sender = admin)
+
+        scenario.h3("set_base_uri impossible with lock")
+        set_and_test_base_uri('https://example3.com/api/', scenario, c1, admin, False)
+
+        scenario.h2("Lock activation when lock is already active")
+
+        scenario.h3("Over-activation")
+        c1.lock().run(sender = admin)
+
+        scenario.h3("set_base_uri still impossible with lock")
+        set_and_test_base_uri('https://example4.com/api/', scenario, c1, admin, False)
+
+        scenario.h2("Lock when all tokens are minted (new contract)")
+        config.max_editions = 1
+        c2 = FA2(config = config,
+            metadata = sp.utils.metadata_of_url("https://example.com"),
+            admin = admin.address)
+        scenario += c2
+
+        scenario.h3("set_base_uri possible when all tokens have been minted")
+        c2.mint(1).run(sender = alice, amount = sp.mutez(1000000))
+        c2.mint(1).run(sender = alice, amount = sp.mutez(1000000), valid=False)
+        set_and_test_base_uri('https://example1.com/api/', scenario, c2, admin)
+
+        scenario.h3("Lock from non-admin")
+        c2.lock().run(sender = alice, valid = False)
+
+        scenario.h3("set_base_uri still possible")
+        set_and_test_base_uri('https://example2.com/api/', scenario, c2, admin)
+
+        scenario.h3("Lock from admin")
+        c2.lock().run(sender = admin, valid = True)
+
+        scenario.h3("set_base_uri impossible with lock")
+        set_and_test_base_uri('https://example3.com/api/', scenario, c2, admin, False)
+
     @sp.add_test(name = "tzip12 tests transfer", is_default = is_default)
     def tests_transfer():
         scenario = sp.test_scenario()
